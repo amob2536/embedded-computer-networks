b904424829ac84a236bb444c8f6a3ccde1218696
amob2536 <56110318+amob2536@users.noreply.github.com>
2019-10-05 11:34:42 +0100

    Lab 1 Excercises Updated
    Lab 1 Excersises have been updated and tested

--- lab_101/3_exercises/src/b_led_dice.c
+++ lab_101/3_exercises/src/b_led_dice.c
@@ -14,8 +14,16 @@
 #include "gpio.h"
 #include "random_numbers.h"
 
+void display(int);
+
 // map the led to gpio pins
-gpio_pin_t led1 = {PA_8,  GPIOA, GPIO_PIN_8};
+gpio_pin_t led1 = {PI_2,  GPIOI, GPIO_PIN_2};
+gpio_pin_t led2 = {PA_15,  GPIOA, GPIO_PIN_15};
+gpio_pin_t led3 = {PA_8,  GPIOA, GPIO_PIN_8};
+gpio_pin_t led4 = {PB_15,  GPIOB, GPIO_PIN_15};
+gpio_pin_t led5 = {PB_14,  GPIOB, GPIO_PIN_14};
+gpio_pin_t led6 = {PI_1,  GPIOI, GPIO_PIN_1};
+
 
 // this is the main method
 int main()
@@ -24,5 +32,76 @@ int main()
   // properly
   HAL_Init();
   init_sysclk_216MHz();
+	
+	// Init random number generator
+	init_random();
+	
+	 // initialise the gpio pins
+  init_gpio(led1, OUTPUT);
+	init_gpio(led2, OUTPUT);
+  init_gpio(led3, OUTPUT);
+	init_gpio(led4, OUTPUT);
+  init_gpio(led5, OUTPUT);
+  init_gpio(led6, OUTPUT);
+	
+	while(1)
+	{		
+		//Generate Random Number
+		uint32_t rnd = (get_random_int() % 6) + 1;
+		
+		display(rnd);
+		
+		// wait for 1 second
+    HAL_Delay(1000);
+		
+	}
+}
 
+void display(int rnd)
+{
+			write_gpio(led1, LOW);
+	    write_gpio(led2, LOW);
+	    write_gpio(led3, LOW);
+	    write_gpio(led4, LOW);
+	    write_gpio(led5, LOW);
+	    write_gpio(led6, LOW);
+	
+			switch(rnd)
+	  	{
+				case 1:
+					toggle_gpio(led1); 
+				break;
+				case 2: 
+					toggle_gpio(led1);
+					toggle_gpio(led2);
+				break;
+				case 3: 
+					toggle_gpio(led1);
+					toggle_gpio(led2);
+					toggle_gpio(led3);
+				break;
+				case 4: 
+					toggle_gpio(led1);
+					toggle_gpio(led2);
+					toggle_gpio(led3);
+					toggle_gpio(led4);			
+				break;
+				case 5: 
+					toggle_gpio(led1);
+					toggle_gpio(led2);
+					toggle_gpio(led3);
+					toggle_gpio(led4);
+					toggle_gpio(led5);			
+				break;
+				case 6: 
+					toggle_gpio(led1);
+					toggle_gpio(led2);
+					toggle_gpio(led3);
+					toggle_gpio(led4);
+					toggle_gpio(led5);
+					toggle_gpio(led6);			
+				break;
+				default:; 
+				break;
+		 }
 }
--- lab_101/4_pushbuttons/src/main.c
+++ lab_101/4_pushbuttons/src/main.c
@@ -20,8 +20,10 @@
 
 // map the led to GPIO PI_1 (the inbuilt led) and the push button to PI_11 
 // (the user button)
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
-gpio_pin_t pb1 = {PI_11, GPIOI, GPIO_PIN_11};
+gpio_pin_t led1 = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led2 = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t led3 = {PB_15, GPIOB, GPIO_PIN_15};
+gpio_pin_t pb1 = {PA_8, GPIOA, GPIO_PIN_8};
 
 // this is the main method
 int main()
@@ -32,22 +34,52 @@ int main()
   init_sysclk_216MHz();
   
   // initialise the gpio pins
-  init_gpio(led, OUTPUT);
+  init_gpio(led1, OUTPUT);
+  init_gpio(led2, OUTPUT);
+	init_gpio(led3, OUTPUT);
   init_gpio(pb1, INPUT);
+	
+	//Init Variables
+	int Debounce = 0;
+	int Led = 1;
+
   
   // loop forever ...
   while(1)
   {
     // if the button is pressed ...
-    if(read_gpio(pb1))
+    if(read_gpio(pb1)==LOW)
     {
-      // turn the led on on the gpio pin
-      write_gpio(led, HIGH);
-    }
-    else
-    {
-      // turn the led off on the gpio pin
-      write_gpio(led, LOW);
-    }
-  }
+			HAL_Delay(5);
+			
+			if(read_gpio(pb1)==LOW)
+			{
+				Debounce = 1;
+			}
+		}
+		if((Debounce == 1)&&(read_gpio(pb1)==HIGH))
+		{
+			switch (Led)
+			{
+				case 1: 
+					write_gpio(led1, HIGH);
+				  write_gpio(led3, LOW);
+				  Led = 2;
+				break;
+				case 2: 
+					write_gpio(led2, HIGH);
+				  write_gpio(led1, LOW);
+				  Led = 3;
+				break;
+				case 3:
+					write_gpio(led3, HIGH);
+				  write_gpio(led2, LOW);
+				  Led = 1;
+				break;
+				default: ;
+				break;
+			}
+		 Debounce = 0;			
+		}
+	}
 }
